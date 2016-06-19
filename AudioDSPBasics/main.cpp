22c531653a52c945450a3b95edb6523362d82bcd
Gerry Beauregard <g.beauregard@ieee.org>
2016-06-19 15:33:25 +0800

    Initial Commit

---
+++ AudioDSPBasics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AudioDSPBasics
+//
+//  Created by Gerry on 2016-06-19.
+//  Copyright © 2016 cognosonic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
